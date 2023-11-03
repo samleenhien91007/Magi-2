8efdf667bf4f39b7bb69772e50dfbb12c8ea7700
Le An Nhien <97014094+samleenhien91007@users.noreply.github.com>
2023-11-03 04:35:44 +0000

    Add logging DEBUG level and resset TOKEN

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from dotenv import load_dotenv
+import logging.handlers
+import logging
 import discord
 import os
-from dotenv import load_dotenv
+
 
 load_dotenv()
 
@@ -9,6 +12,23 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+# DEBUG logging except for HTTP requests
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+logging.getLogger('discord.http').setLevel(logging.INFO)
+
+handler = logging.handlers.RotatingFileHandler(
+    filename='discord.log',
+    encoding='utf-8',
+    maxBytes=32 * 1024 * 1024,  # 32 MiB
+    backupCount=5,  # Rotate through 5 files
+)
+dt_fmt = '%Y-%m-%d %H:%M:%S'
+formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+# Main code block for bot function
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
